285df0865d6a8e21a97d46b178c4c2fe9b0407cc
Darkmen243 <evseenko.vowa@yandex.ru>
2026-01-23 20:19:50 +0300

    finished 5th exercise and added first FastAPI task

--- Decent Tasks/5.py
+++ Decent Tasks/5.py
@@ -1,10 +1,18 @@
 '''
-Find first non-repeating character in a string
+1) Find first non-repeating character in a string
 
-Find most frequent word in text
+2) Find most frequent word in text
 
-Merge overlapping ranges
+3) Merge overlapping ranges
+[(1, 5), (3, 7), (8, 10)]
+
+Some overlap, some don’t.
+
+You must merge overlapping ones so the result becomes:
+[(1, 7), (8, 10)]
 '''
+
+#1
 test_string = "aabbcdde"
 char_dict = {}
 for c in test_string:
@@ -16,4 +24,38 @@ for item in char_dict:
     if char_dict[item]==1:
         print(f"First non-repeating character is {item}")
         break
+#2
+word_dict = {}
+max_occ = 0
+max_word = ""
+try:
+    with open("Decent Tasks/3.txt") as file:
+        for line in file:
+            for word in line.split():
+                if word not in word_dict:
+                    word_dict[word] = 1
+                else:
+                    word_dict[word]+=1
+        
+        for word in word_dict:
+            if word_dict[word]>max_occ:
+                max_occ = word_dict[word]
+                max_word = word
+        print(f"Most frequent word in text is {max_word}")
+except Exception as ex:
+    print(ex)
 
+#3
+test_ranges = [(1, 5), (3, 7), (8, 10)]
+test_ranges.sort()
+merged_ranges = []
+current_range = list(test_ranges[0])
+for range in test_ranges:
+    if current_range[1]>=range[0]:
+        current_range[1] = max(current_range[1], range[1]) 
+    else:
+        merged_ranges.append((current_range[0],current_range[1]))
+        print(current_range)
+        current_range = list(range)
+merged_ranges.append((current_range[0],current_range[1]))    
+print(merged_ranges)
---
+++ FastAPI/main.py
@@ -0,0 +1,53 @@
+'''
+Create FastAPI app with:
+
+POST /user
+
+GET /users
+
+GET /users/{id}
+
+Use Pydantic models.
+'''
+import uuid
+from fastapi import FastAPI, Body, status
+from fastapi.responses import JSONResponse, FileResponse
+from pydantic import BaseModel, ConfigDict
+
+app = FastAPI()
+
+class UserCreate(BaseModel):
+    name: str
+
+class UserRead(BaseModel):
+    id: str
+    name: str
+
+Users = {}
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.post("/user")
+def create_user(user:UserCreate):
+    user_id  = str(uuid.uuid4())
+    new_user = {
+        "id": user_id,
+        "name": user.name
+    }
+    Users[user_id] = new_user
+    return new_user
+@app.get("/users")
+def get_users():
+    return Users
+@app.get("/users/{id}", response_model=UserRead)
+def get_user(id:str):
+    user = Users.get(id)
+    if not user:
+           return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Пользователь не найден" }
+        )
+    else:
+        return user
